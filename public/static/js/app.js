de0f6ad06a444a181111092a327238a5dd46c7e7
jmpainter <jpainter4@gmail.com>
2018-09-18 10:37:38 -0700

    fixed navigation bar

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,56 +1,9 @@
-let API_URL = window.location.protocol + '//' + window.location.hostname;
-if(window.location.port) {
-  API_URL = API_URL + `:${window.location.port}`;
-}
-
-const appState = {
-  userInfo: null,
-  latestDreams: [],
-  journalDreams: [],
-  currentDream: null,
-  currentPage: 1,
-  totalPages: null,
-  editorMode: '',
-  viewStack: []
-};
-
-//used for returning from POST PUT or DELETE where updated data must be shown
-function initPreviousScreen() {
-  //pop off currend screen;
-  appState.viewStack.pop();
-  //get previous screen
-  const prevScreen = appState.viewStack.pop();
-  if(prevScreen === 'recent-dreams') {
-    initRecentDreams();
-  } else if (prevScreen === 'dream-detail') {
-    initDreamDetail();
-  } else if (prevScreen === 'dream-editor') {
-    initDreamEditor();
-  } else if (prevScreen === 'dream-journal') {
-    initDreamJournal();
-  }
-}
-
-//used for back button when no initialization of prevous screen is necessary
-function returnToPreviousScreen() {
-  //pop off currend screen;
-  appState.viewStack.pop();
-  //get previous screen
-  const prevScreen = appState.viewStack[appState.viewStack.length - 1];
-  showView(prevScreen);
-}
-
-function showView(viewName) {
-  $('.view').css('display', 'none');
-  $('#' + viewName).css('display', 'block');
-}
-
 function toggleNav(){
-  const nav = $('.top-nav');
-  if (nav.attr('class') === 'top-nav'){
-    nav.attr('class', 'top-nav responsive');
+  const nav = $('.nav-links');
+  if (nav.attr('class') === 'nav-links'){
+    nav.attr('class', 'nav-links responsive');
   } else {
-    nav.attr('class', 'top-nav');
+    nav.attr('class', 'nav-links');
   }
 }
 
@@ -86,15 +39,8 @@ function handleToggleNav() {
 }
 
 function startApp() {
-  if(Cookies.get('_dream-catcher-token')) {
-    setMenu('user');
-    getJournalDreams(setJournalDreams);
-    getUserInfo(getUserOnAppStartError);
-  } else {
-    setMenu('public');
-  }
+  setMenu('public');
   handleToggleNav();
-  initRecentDreams();
 }
 
 $(startApp);
