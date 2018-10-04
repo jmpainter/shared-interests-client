aed55573fa9d280e35b6fab5530da181ce772512
jmpainter <jpainter4@gmail.com>
2018-10-03 19:17:40 -0700

    added test for refreshAuthToken

--- src/actions/auth.test.js
+++ src/actions/auth.test.js
@@ -10,7 +10,8 @@ import {
   authSuccess,
   AUTH_ERROR,
   authError,
-  login
+  login,
+  refreshAuthToken
 } from './auth';
 import {API_BASE_URL} from '../config';
 
@@ -68,13 +69,37 @@ describe('login', () => {
       });
     });
     const dispatch = jest.fn();
-
     const decodedToken = jwtDecode(authToken);
     
     return login('sam@gmail.com', 'password')(dispatch).then(() => {
       expect(fetch).toHaveBeenCalledWith(`${API_BASE_URL}/auth/login`,  {"body": "{\"username\":\"sam@gmail.com\",\"password\":\"password\"}", "headers": {"Content-Type": "application/json"}, "method": "POST"});
       expect(dispatch).toHaveBeenCalledWith(authSuccess(decodedToken.user));
     });
+  });
+});
 
+describe('refreshAuthToken', () => {
+  it('Should dispatch authSuccess', () => {
+    const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWJiNTczY2I0NGYyNDkzNjU4OTAxYmUwIiwiZmlyc3ROYW1lIjoiU2FtIiwibGFzdE5hbWUiOiJSZXlub2xkcyIsInNjcmVlbk5hbWUiOiJzcmVub2xkcyIsImxvY2F0aW9uIjoiQmVya2VsZXksIENBLCBVU0EiLCJ1c2VybmFtZSI6InNhbUBnbWFpbC5jb20ifSwiaWF0IjoxNTM4NjE4Mzc3LCJleHAiOjE1MzkyMjMxNzcsInN1YiI6InNhbUBnbWFpbC5jb20ifQ.2RqwJPuOmvPb5k4T-9HoEYfH4-AyLNGQwqjM43Bu11o';
+
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json() {
+          return { authToken }
+        }
+      });
+    });
+    const dispatch = jest.fn();
+    const decodedToken = jwtDecode(authToken);
+
+    const getState = () => {
+      return { auth: { authToken }}
+    }
+    
+    return refreshAuthToken('sam@gmail.com', 'password')(dispatch, getState).then(() => {
+      expect(fetch).toHaveBeenCalledWith(`${API_BASE_URL}/auth/refresh`,  {"headers": {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWJiNTczY2I0NGYyNDkzNjU4OTAxYmUwIiwiZmlyc3ROYW1lIjoiU2FtIiwibGFzdE5hbWUiOiJSZXlub2xkcyIsInNjcmVlbk5hbWUiOiJzcmVub2xkcyIsImxvY2F0aW9uIjoiQmVya2VsZXksIENBLCBVU0EiLCJ1c2VybmFtZSI6InNhbUBnbWFpbC5jb20ifSwiaWF0IjoxNTM4NjE4Mzc3LCJleHAiOjE1MzkyMjMxNzcsInN1YiI6InNhbUBnbWFpbC5jb20ifQ.2RqwJPuOmvPb5k4T-9HoEYfH4-AyLNGQwqjM43Bu11o"}, "method": "POST"});
+      expect(dispatch).toHaveBeenCalledWith(authSuccess(decodedToken.user));
+    });
   });
 });
