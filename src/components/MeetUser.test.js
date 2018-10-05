55105df632843962f0f671de8d0c5ae5ccbdf281
jmpainter <jpainter4@gmail.com>
2018-10-05 12:45:11 -0700

    fixed tests

--- src/components/MeetUser.test.js
+++ src/components/MeetUser.test.js
@@ -24,9 +24,6 @@ const mockGetOtherUser = {
 jest.mock('../actions/users', () => Object.assign({},
   require.requireActual('../actions/users'),
   {
-    putUserInfoAndGetUserInfo: jest.fn().mockImplementation(() => {
-        return mockPutUserInfoAndGetUserInfo;
-    }),
     getOtherUser: jest.fn().mockImplementation(() => {
         return mockGetOtherUser;
     })
@@ -47,16 +44,6 @@ describe('<MeetUser />', () => {
     const dispatch = jest.fn();
     const match = { params: { id: 'fakeId' } };
     shallow(<MeetUser store={store} dispatch={dispatch} match={match}list={[]} />);
-  });
-
-  it('Should block a user', () => {
-    const dispatch = jest.fn();
-    // send in the user id parameter which would have been in the URL
-    const match = { params: { id: 'fakeId' } };
-    const wrapper =  shallow(<MeetUser store={store} isTest={true} history={[]} user={initialState.user.user} dispatch={dispatch} match={match} list={[]} />);
-    wrapper.find('.block-user').simulate('click');
-    // make sure the last call to dispatch is for the correct thunk
-    expect(dispatch).toHaveBeenLastCalledWith(mockPutUserInfoAndGetUserInfo);
   });
 
   it('Should start a conversation', () => {
--- src/components/MessageThread.test.js
+++ src/components/MessageThread.test.js
@@ -13,7 +13,7 @@ describe('<MessageThread />', () => {
   it('Renders a list of interests', () => {
     const messages = initialState.conversations.conversations[0].messages;
     const wrapper = shallow(<MessageThread messages={messages} />);
-    expect(wrapper.html()).toEqual('<div class=\"message-thread\"><h3>Conversation:</h3><div><div class=\"message-text\">Hello there</div><span class=\"message-sender\">josh - </span><span class=\"message-date\">9/12/2018</span></div><div><div class=\"message-text\"><p>This is a reply from amy</p></div><span class=\"message-sender\">amy - </span><span class=\"message-date\">9/27/2018</span></div><div><div class=\"message-text\"><p>Another message from Josh</p></div><span class=\"message-sender\">josh - </span><span class=\"message-date\">9/27/2018</span></div></div>');
+    expect(wrapper.html()).toEqual('<div class=\"message-thread\"><div><div class=\"message-text\">Hello there</div><span class=\"message-sender\">josh - </span><span class=\"message-date\">9/12/2018</span></div><div><div class=\"message-text\"><p>This is a reply from amy</p></div><span class=\"message-sender\">amy - </span><span class=\"message-date\">9/27/2018</span></div><div><div class=\"message-text\"><p>Another message from Josh</p></div><span class=\"message-sender\">josh - </span><span class=\"message-date\">9/27/2018</span></div></div>');
   });
 
 });
